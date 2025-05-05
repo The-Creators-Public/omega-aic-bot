a51019e42a257c908c533a3cc1e9e576f7921bda
CodeMaster013 <160362417+CodeMaster013@users.noreply.github.com>
2025-05-05 19:18:35 -0400

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,1 +1,10 @@
+const { SlashCommandBuilder } = require('discord.js');
 
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('ping')
+    .setDescription('Omega.aic will respond with system latency.'),
+  async execute(interaction) {
+    await interaction.reply('Pong. Latency nominal.');
+  },
+};
