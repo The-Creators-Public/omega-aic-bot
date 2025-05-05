5ba159cc607edfafdef48e4c08932ac72c582658
CodeMaster013 <160362417+CodeMaster013@users.noreply.github.com>
2025-05-05 19:17:19 -0400

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,1 +1,26 @@
+const { REST, Routes } = require('discord.js');
+const { readdirSync } = require('fs');
+const { TOKEN, CLIENT_ID } = require('dotenv').config().parsed;
 
+const commands = [];
+const commandFiles = readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+  commands.push(command.data.toJSON());
+}
+
+const rest = new REST({ version: '10' }).setToken(TOKEN);
+
+(async () => {
+  try {
+    console.log('Deploying commands...');
+    await rest.put(
+      Routes.applicationCommands(CLIENT_ID),
+      { body: commands },
+    );
+    console.log('Commands registered.');
+  } catch (error) {
+    console.error(error);
+  }
+})();
